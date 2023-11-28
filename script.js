0d34fffede2ed8ff4313e7b1564f7b113bd72b35
tika-woww <145320307+tika-woww@users.noreply.github.com>
2023-11-29 00:16:29 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ function asalMengoakan() {
 
 function penjelasanMegoakan() {
     var replaceText = document.getElementsByClassName("penjelasan-kebudayaan");
-    replaceText[0].innerHTML = "Tari Kecak sering dipentaskan di berbagai tempat wisata di Bali, seperti Uluwatu dan Ubud. Pertunjukan ini sangat populer di kalangan wisatawan dan menjadi salah satu atraksi budaya terkenal di Bali. kebudayaan ini unik karena tidak menggunakan instrumen musik tradisional Bali seperti gamelan.Sebaliknya, dengan mengunakan suara vocal yang dinyanyikan oleh sekelompok penari pria yang duduk dalam formasi lingkaran."
+    replaceText[0].innerHTML = "Berupa permainan tradisional yang biasanya dilakukan oleh anak-anak, Megoak-goakan sendiri di inspirasi oleh seekor burung goak (gagak) yang sedang mengincar mangsanya. Tradsi Megoak-goakan di desa Panji Buleleng ini digelar setiap tahun untuk menghormati jasa dari raja Ki Barak Panji Sakti. ";
 }
 
 // JOGED BUMBUNG
@@ -79,28 +79,28 @@ function judulGebugEnde() {
 
 function asalGebugEnde() {
     var replaceText = document.getElementsByClassName("asal-kebudayaan");
-    replaceText[0].innerHTML = "Berasal dari Desa Banyumas";
+    replaceText[0].innerHTML = "Dikembangkan dari Desa Pejarakan";
 }
 
 function penjelasanGebugEnde() {
-    var replaceText = document.getElementsByClassName("penjelasan-kebudayaan");
-    replaceText[0].innerHTML = "Joged Bumbung merupakan kebudayaan yang meriah dan menghibur yang mencerminkan semangat hidup masyarakat Bali. kebudayaan ini sering dipentaskan dalam berbagai acara, termasuk upacara adat, perayaan budaya, dan pertunjukan seni di Bali. kebudayaan ini juga sering menjadi daya tarik wisata di Bali."
+    var replaceText = document.getElementsByClassName("penjelasan-kebudayaan2");
+    replaceText[0].innerHTML = "Gebug Ende berasal dari 2 kata, gebug yang berarti pukul dan Ende yang berarti perisai dari kulit. Secara etimologi, tradisi Gebug Ende berarti permainan yang dilakukan 2 kelompok yang saling menyerang dengan alat khusus. Tradisi Gebug Ende pada dasarnya dilakukan dalam bentuk pertandingan, di mana peserta akan saling menyerang dan menangkis. ";
 }
 
 //BARIS
 function judulNgesubaBukaka() {
     var replaceText = document.getElementsByClassName("judul-kebudayaan");
-    replaceText[0].innerHTML = "Ngesuba Bukakak";
+    replaceText[0].innerHTML = "Ngusuba Bukakak";
 }
 
 function asalNgesubaBukaka() {
     var replaceText = document.getElementsByClassName("asal-kebudayaan");
-    replaceText[0].innerHTML = "Berasal dari Bali";
+    replaceText[0].innerHTML = "Berasal dari Desa Giri Mas";
 }
 
 function penjelasanNgesubaBukaka() {
-    var replaceText = document.getElementsByClassName("penjelasan-kebudayaan");
-    replaceText[0].innerHTML = "kebudayaan ini mencerminkan nilai-nilai keberanian, persatuan, dan patriotisme dalam budaya Bali. Selain itu, Tari Baris juga sering digunakan dalam upacara adat untuk menghormati roh leluhur. kebudayaan ini biasanya ditampilkan oleh sekelompok penari pria yang mengenakan kostum prajurit klasik Bali, termasuk pakaian dan peralatan perang tradisional."
+    var replaceText = document.getElementsByClassName("penjelasan-kebudayaan2");
+    replaceText[0].innerHTML = "Tradisi unik di Buleleng berikutnya adalah Ngusaba Bukakak. Tradisi unik ini berasal dari desa Giri Emas, Kecamatan Sawan. Desa tersebut terkenal akan hasil bumi yang melimpah. Tradisi ini bertujuan untuk menyampaikan terima kasih kepada Ida Sang Hyang Widhi Wasa atas kesuburan tanah dan hasil pertanian yang melimpah. Upacara ini dilakukan setiap 2 tahun sekali setiap bulan April atau pada bulan purnama sasih kedasa menurut kalender Bali.";
 }
 
 // PENDET
@@ -112,12 +112,12 @@ function judulNyakanDiwan() {
 
 function asalNyakanDiwan() {
     var replaceText = document.getElementsByClassName("asal-kebudayaan");
-    replaceText[0].innerHTML = "Berasal dari Klukung";
+    replaceText[0].innerHTML = "Berasal dari Banjar";
 }
 
 function penjelasanNyakanDiwan() {
-    var replaceText = document.getElementsByClassName("penjelasan-kebudayaan");
-    replaceText[0].innerHTML = "Tari Pendet berasal dari Desa Sidhakarya, Kabupaten Klungkung, Bali, yang berkembang di Bali. Awalnya diciptakan sebagai ungkapan selamat datang untuk dewa. Seiring berjalannya waktu, kebudayaan ini berkembang menjadi pertunjukan seni dan digunakan dalam berbagai konteks, seperti kebudayaan penyambutan."
+    var replaceText = document.getElementsByClassName("penjelasan-kebudayaan2");
+    replaceText[0].innerHTML = "Sebuah tradisi unik Kecamatan Banjar Kabupaten Buleleng di namakan Nyakan Diwang tradisi ini di kenal dengan menanak nasi di luar rumah tepatnya di pinggir jalan di Desa Banjar. Menurut kisah Leluhur Nyakan Diwang ini merupakan rangkaian perayaan Hari Raya Nyepi. Yang di nyatakan sebagai bentuk pembersihan rumah terutama penyepian dapur setiap keluarga di desa banjar.";
 }
 
 // LEGONG
@@ -129,10 +129,10 @@ function judulSapiGerumbungan() {
 
 function asalSapiGerumbungan() {
     var replaceText = document.getElementsByClassName("asal-kebudayaan");
-    replaceText[0].innerHTML = "Dikembangkan di Gianyar dan Badung";
+    replaceText[0].innerHTML = "Berasal dari Kaliasem";
 }
 
 function penjelasanSapiGerumbungan() {
-    var replaceText = document.getElementsByClassName("penjelasan-kebudayaan");
-    replaceText[0].innerHTML = "Tari Legong di Bali adalah sebuah seni pertunjukan yang mencerminkan nilai-nilai agama dan sejarah yang melambangkan ungkapan rasa syukur dan terima kasih terhadap nenek moyang. Tari Legong juga merupakan tarian hiburan yang digunakan dalam penyambutan tamu dan menjadi daya tarik bagi wisatawan."
+    var replaceText = document.getElementsByClassName("penjelasan-kebudayaan2");
+    replaceText[0].innerHTML = "Sampi Gerumbungan merupakan salah satu tradisi khas Buleleng, Bali. Tradisi ini rutin dilaksanakan oleh para petani sebagai wujud ungkapan syukur atas hasil garapan yang melimpah. Sampi Gerumbungan lebih menekankan unsur keindahan, keserasian melalui gerakan sapi yang selaras, dan menonjolkan postur sapi yang baik, tegak, dan gagah.";
 }
